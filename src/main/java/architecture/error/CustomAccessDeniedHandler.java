edc296c844f02106c1d8afc9b88fc0c832b1ec67
Andrey Minev <andrey_st_minev@yahoo.com>
2020-08-08 15:51:34 +0300

    rem import

--- src/main/java/architecture/error/CustomAccessDeniedHandler.java
+++ src/main/java/architecture/error/CustomAccessDeniedHandler.java
@@ -1,6 +1,5 @@
 package architecture.error;
 
-import architecture.constants.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
